e1a66906d6977aab620b7b902d836fb7e416e026
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-06-21 15:26:28 -0700

    Time: 73 ms (32.73%), Space: 16.9 MB (59.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,5 +2,24 @@
 ## space:o(s+t)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        
+        if(len(s) != len(t)):
+            return False;
+        
+        mapS={}
+        mapT={}
+        
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i],0)
+            mapT[t[i]] = 1 + mapT.get(t[i],0)
+            
+        return mapS == mapT 
+    
+    
+# Get user input for s and t
+##s = input("Enter the first string: ")
+##t = input("Enter the second string: ")
+
+##solution = Solution()
+##print(solution.isAnagram(s, t))    
         
