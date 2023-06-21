9d43e78901406735871e3043db1035301928f2e0
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-06-21 14:32:18 -0700

    Time: 150 ms (57.26%), Space: 69.6 MB (45.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,42 +1,44 @@
-// brute force
-//time complexity = o(nlogn)
-//space complexity = o(1)
-
+//optimized approach
+//time:o(1)
+//space:o(n)
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]){
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i]) != s.end()){
                 return true;
             }
+            s.insert(nums[i]);
         }
         return false;
-        
     }
 };
 
+
 /*
-int main() {
+int main(){
     int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
+    cout<<"enter size of array"<<endl;
+    cin>>n;
+    
     vector<int> nums(n);
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+    
+    cout<<"enter elements"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
     }
-
+    
     Solution solution;
+    
     bool containsDup = solution.containsDuplicate(nums);
-
-    if (containsDup) {
-        cout << "true" << endl;
-    } else {
-        cout << "false" << endl;
+    
+    if(containsDup){
+        cout<<"true"<<endl;
     }
-
+    else{
+        cout<<"false"<<endl;
+    }
     return 0;
 }
 */
