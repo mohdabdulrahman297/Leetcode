fcf3b46053677dffa9bba0c4dd2e043d12955e52
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-06-25 22:35:25 -0700

    Time: 63 ms (87.42%), Space: 16.6 MB (15.99%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        
+        ## create a hashset to store unique stringd
+        unique = set()
+        
+        for e in emails:
+            ## split the string before @ symbol as local and after @ symbol as domain
+            local,domain = e.split("@")
+            ##consider local that are before + sign and 0 indicates just before the + sign
+            local = local.split("+")[0]
+            local = local.replace("." , "")
+            unique.add((local,domain))
+            
+        return len(unique)     
+        
