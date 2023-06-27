fec5c156d28b9053924e31db6bc7044beb91ce01
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-06-26 23:48:41 -0700

    Time: 123 ms (99.27%), Space: 15 MB (36.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -13,5 +13,20 @@ class Solution(object):
                 result =i
             ## increament count by 1 if result = element else decreament count by 1
             count += (1 if i == result else -1)
-        return result    
+        return result 
+    
+ # Create an instance of the Solution class
+##solution = Solution()
+
+# Test case 1
+##nums1 = [1, 2, 2, 2, 3]
+##print(solution.majorityElement(nums1))  # Output: 2
+
+# Test case 2
+##nums2 = [3, 3, 4, 2, 4, 4, 2, 4, 4]
+##print(solution.majorityElement(nums2))  # Output: 4
+
+# Test case 3
+##nums3 = [1, 1, 1, 2, 3, 4]
+##print(solution.majorityElement(nums3))  # Output: 1   
         
