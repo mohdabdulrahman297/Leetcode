a00b123b0aa4aace7e24a8c682a22857eaba32b4
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-06-26 23:16:14 -0700

    Time: 19 ms (70.13%), Space: 20.3 MB (25.28%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -19,3 +19,26 @@ public:
         
     }
 };
+
+/*
+int main() {
+    Solution solution;
+
+    // Test case 1
+    vector<int> flowerbed1 = {1, 0, 0, 0, 1};
+    int n1 = 1;
+    cout << boolalpha << solution.canPlaceFlowers(flowerbed1, n1) << endl;  // Output: true
+
+    // Test case 2
+    vector<int> flowerbed2 = {1, 0, 0, 0, 1};
+    int n2 = 2;
+    cout << boolalpha << solution.canPlaceFlowers(flowerbed2, n2) << endl;  // Output: false
+
+    // Test case 3
+    vector<int> flowerbed3 = {0, 0, 1, 0, 1};
+    int n3 = 1;
+    cout << boolalpha << solution.canPlaceFlowers(flowerbed3, n3) << endl;  // Output: true
+
+    return 0;
+}
+*/
