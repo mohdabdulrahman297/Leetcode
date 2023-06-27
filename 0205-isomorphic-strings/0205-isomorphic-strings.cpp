875bdcd8e5d0e0bd04f269773f6ec1ed7747733b
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-06-26 22:33:31 -0700

    Time: 20 ms (6.43%), Space: 9.2 MB (5.48%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,37 @@
-/*Given two strings s and t, determine if they are isomorphic.
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
- */
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       unordered_map<char,vector<int>>m1;
-         unordered_map<char,vector<int>>m2;
-        for(int i=0;i<s.length();i++){
-            m1[s[i]].push_back(i);
-             m2[t[i]].push_back(i);
-            
-            if(m1[s[i]]!=m2[t[i]])
+        unordered_map<char, vector<int>> mapST, mapTS;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            mapST[c1].push_back(i);
+            mapTS[c2].push_back(i);
+
+            if (mapST[c1] != mapTS[c2]) {
                 return false;
+            }
         }
         return true;
-        
     }
 };
+
+/*
+int main() {
+    Solution solution;
+
+    string s = "egg";
+    string t = "add";
+
+    cout << solution.isIsomorphic(s, t) << endl;
+
+    return 0;
+}
+*/
