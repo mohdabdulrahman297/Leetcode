e5da5b5b7f1a348c7bdddd6d4d3b323ad5da9bf2
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-07-03 13:17:31 +0530

    Time: 3 ms (95.17%), Space: 8.9 MB (33.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,36 +3,39 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
-        unordered_map<int , int> nums1Idx;
-        
-        for(int i=0;i<nums1.size();i++){
+        // Creating a map nums1Idx and mapping values to their indices
+        unordered_map<int, int> nums1Idx;
+        for (int i = 0; i < nums1.size(); i++) {
             nums1Idx[nums1[i]] = i;
-            
         }
+
+        // Create a result vector of size nums1 to store the output
+        vector<int> res(nums1.size(), -1);
         
+        // Create an empty stack
+        stack<int> st;
         
-        vector<int> result;
-        for(int i=0;i<nums1.size();i++){
-            result.push_back(-1);
-        }
-        
-        stack<int> stack;
-        
-        for(int i=0;i<nums2.size();i++){
+        for (int i = 0; i < nums2.size(); i++) {
             int curr = nums2[i];
-            while(!stack.empty() && curr > stack.top()){
-                int val = stack.top();
-                stack.pop();
+            
+            // Check if stack is not empty and curr is greater than the top element of the stack
+            while (!st.empty() && curr > st.top()) {
+                int val = st.top();
+                st.pop();
+                
+                // Find the index of the value through the map
                 int idx = nums1Idx[val];
                 
-                result[idx] = curr;
+                // Store the next greater element, i.e., curr, at the index correlated to nums1
+                res[idx] = curr;
             }
             
-            if(nums1Idx.find(curr) != nums1Idx.end()){
-                stack.push(curr);
+            // Check if curr (next greater element) exists in nums1 and add it to the stack
+            if (nums1Idx.find(curr) != nums1Idx.end()) {
+                st.push(curr);
             }
         }
-        return result;
+        
+        return res;
     }
 };
