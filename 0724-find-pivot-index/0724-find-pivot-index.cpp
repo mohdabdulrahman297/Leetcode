51c35ceb1fbacff466d75b20ced73f85cc8b4bff
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-07-03 13:44:04 +0530

    Time: 29 ms (50.93%), Space: 31 MB (82.40%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -19,3 +19,26 @@ public:
         return -1;
     }
 };
+
+/*
+int main() {
+    Solution solution;
+
+    // Example 1
+    vector<int> nums = {1, 7, 3, 6, 5, 6};
+    int output = solution.pivotIndex(nums);
+    cout << output << endl;  // Output: 3
+
+    // Example 2
+    nums = {1, 2, 3};
+    output = solution.pivotIndex(nums);
+    cout << output << endl;  // Output: -1
+
+    // Custom Test Case
+    nums = {1, 2, 3, 4, 0, 6};
+    output = solution.pivotIndex(nums);
+    cout << output << endl;  // Output: 3
+
+    return 0;
+}
+*/
