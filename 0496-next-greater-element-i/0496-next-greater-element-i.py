544940c689956ca8b9e754e12a100b7ab5414e2e
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-07-03 12:59:19 +0530

    Time: 86 ms (36.18%), Space: 16.7 MB (34.41%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,31 @@
+#time: o(n+m)
+#space: o(n+m)
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Creating a map nums1Idx and mapping values to their indices
+        nums1Idx = {n: i for i, n in enumerate(nums1)}
+        
+        # Create a result vector of size nums1 to store the output
+        res = [-1] * len(nums1)
+        
+        # Create an empty stack
+        stack = []
+        
+        for i in range(len(nums2)):
+            curr = nums2[i]
+            
+            # Check if stack is not empty and curr is greater than the top element of the stack
+            while stack and curr > stack[-1]:
+                val = stack.pop()
+                
+                # Find the index of the value through the map
+                idx = nums1Idx[val]
+                
+                # Store the next greater element, i.e., curr, at the index correlated to nums1
+                res[idx] = curr
+            
+            # Check if curr (next greater element) exists in nums1 and add it to the stack
+            if curr in nums1:
+                stack.append(curr)
+        
+        return res
