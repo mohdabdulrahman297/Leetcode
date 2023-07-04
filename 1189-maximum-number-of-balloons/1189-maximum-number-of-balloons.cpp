f4386d61e954c27560021fa4b05e57ac7caafb0a
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-07-04 12:30:06 +0530

    Time: 4 ms (54.90%), Space: 6.7 MB (29.95%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        map<char, int> countText;
+        map<char, int> balloon;
+        for(char c: text)
+            countText[c]++;
+        for(char c: std::string("balloon"))
+            balloon[c]++;
+        
+        int res = text.length();
+        for(const auto &[key, value]: balloon)
+            res = min(res, countText[key] / value);
+        return res;
+    }
+};
+
+/*
+int main() {
+    Solution solution;
+    string text = "balloonballoonballoon";
+    int result = solution.maxNumberOfBalloons(text);
+    cout << result << endl;
+
+    return 0;
+}
+*/
