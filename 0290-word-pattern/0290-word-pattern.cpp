cf577629bf108b354d1494f9ef0cda7aadd4601d
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-07 19:57:04 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (87.91%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,28 +1,57 @@
-## time: o(n+m)
-## space: o(n+m)
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
+// time: o(n+m)
+// space: o(n+m)
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+         // This creates a stringstream object named str using the input string s. This object is used to extract words from the string.
+         stringstream str(s);
+         //  This declares a string variable named word which will be used to store individual words extracted from the stringstream.
+         string word ; 
+         // This initializes an empty vector named words. This vector will store the individual words extracted from the input string s.
+         vector<string> words;
         
-        ## split the string based on spaces
-        words = s.split(" ")
-        if(len(pattern) != len(words)):
-            return False
         
-        ## use two hashmaps to map character to word and word to character to avoid overlapping
-        CharToWord = {}
-        WordToChar = {}
+         while(str >> word){
+             words.push_back(word);
+         }
         
-        ## if pattern and words are of same length iterate through them at once
-        for c , w in zip(pattern , words):
-            if c in CharToWord and CharToWord[c] != w:
-                return False
-            if w in WordToChar and WordToChar[w] != c:
-                return False
+        if(pattern.size() != words.size()){
+            return false;
+        }
+        
+        unordered_map<char , string> CharToWord;
+        unordered_map<string , char> WordToChar;
+        
+        for(int i=0;i<pattern.size();i++){
+            char c = pattern[i];
+            string w = words[i];
             
-            ## now simple map
-            CharToWord[c] = w
-            WordToChar[w] = c
+            if(CharToWord.find(c) == CharToWord.end() && WordToChar.find(w) == WordToChar.end()){
+                
+                CharToWord[c] = w;
+                WordToChar[w] = c;
+            }
+            else if(CharToWord[c] != w && WordToChar[w] != c){
+                return false;
+            }
+    
             
-        ## now we have iterated through the loop 
-        return True
+        }
+        return true;
         
+        
+             
+            
+    }
+};
+
+
+/*
+int main() {
+    Solution solution;
+    std::cout << solution.wordPattern("abba", "dog cat cat dog") << std::endl;  // Output: 1 (true)
+    std::cout << solution.wordPattern("abba", "dog cat cat fish") << std::endl; // Output: 0 (false)
+    
+    return 0;
+}
+*/
