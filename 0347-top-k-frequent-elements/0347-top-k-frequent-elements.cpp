370fab1c090eeec343fb3362d0653612a81cc827
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-10 04:20:30 +0530

    Time: 7 ms (98.98%), Space: 15.4 MB (8.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,34 +1,33 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ## create a hashmap to map count of occurences
-        count = {}
-        ## create a empty array named frequency
-        freq = [[] for i in range(len(nums) + 1)]
+// space:o(n)
+// time:o(n)
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
         
-        ## count occurences of elements in input array and append it into the newly created array
-        for n in nums:
-            count[n] = 1 + count.get(n,0)
-        for n , c in count.items():
-            freq[c].append(n)
-            
-            
-            
-        result = []
-        ## looping  in reverse order(-1 indicates reverse looping)
-        for i in range(len(freq)-1 , 0 , -1):
-            for n in freq[i]:
-                result.append(n)
-                if(len(result) == k):
-                    return result
-   # Example input
-#nums = [1, 1, 1, 2, 2, 3]
-#k = 2
-
-# Create an instance of the Solution class
-#solution = Solution()
-
-# Call the topKFrequent method to find the top k frequent elements
-#top_k_frequent = solution.topKFrequent(nums, k)
-
-# Print the result
-#print(top_k_frequent)
+        unordered_map<int , int> count;
+        vector<vector<int>> freq(nums.size()+1);
+        
+        for(int n : nums){
+            count[n]++;
+        }
+        
+        for(auto it : count){
+            int num = it.first;
+            int frequency = it.second;
+            freq[frequency].push_back(num);
+        }
+        
+        vector<int> result;
+        
+        for(int i = freq.size()-1 ;  i > 0 ; i--){
+            for(int n : freq[i]){
+                result.push_back(n);
+                if(result.size() == k){
+                    return result;
+                }
+            }
+        }
+        return result;
+        
+    }
+};
