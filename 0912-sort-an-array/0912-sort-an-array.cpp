795d6a4c798bb1b02ef8f1606b4aa8ceae35a146
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-10 03:02:35 +0530

    Time: 510 ms (42.73%), Space: 181 MB (27.25%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void Merge(vector<int> & arr, int L, int M , int R){
+        vector<int> left(arr.begin() + L , arr.begin() + M+1);
+        vector<int> right(arr.begin() + M+1 , arr.begin() + R+1);
+            
+        int i = L;
+        int j =0 ;
+        int k =0;
+        
+        while(j<left.size() && k<right.size()){
+            if(left[j] <= right[k]){
+                arr[i] = left[j];
+                j++;
+            }
+            else{
+                arr[i] = right[k];
+                k++;
+            }
+            i++;
+        }
+        
+        while(j<left.size()){
+            arr[i] = left[j];
+            j++;
+            i++;
+        }
+        while(j<right.size()){
+            arr[i] = right[k];
+            k++;
+            i++;
+        }
+    }
+    void MergeSort(vector<int>& arr , int l , int r){
+        if(l == r){
+            return;
+        }
+        
+        int m = (l+r)/2;
+        
+        MergeSort(arr , l , m);
+        MergeSort(arr , m+1 , r);
+        
+        Merge(arr, l , m , r);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        MergeSort(nums , 0 , nums.size()-1);
+        return nums;
+        
+    }
+};
