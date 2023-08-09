3432a984bf7680f418d8f34202b32108d23a8c79
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-09 10:18:54 +0530

    Time: 107 ms (92.00%), Space: 208.2 MB (11.29%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,49 +1,24 @@
-class ListNode:
-    def __init__(self , key = -1 , val = -1 , next = None):
-        self.key = key
-        self.val = val
-        self.next = next
-class MyHashMap:
+class MyHashMap {
+public:
+    int data[1000001];
+    MyHashMap() {
+        fill(data, data + 1000000, -1);
+    }
+    void put(int key, int val) {
+        data[key] = val;
+    }
+    int get(int key) {
+        return data[key];
+    }
+    void remove(int key) {
+        data[key] = -1;
+    }
+};
 
-    def __init__(self):
-        ## create an array of listnode with 1000 range
-        self.map = [ListNode() for i in range(1000)]
-        
-
-    def put(self, key: int, value: int) -> None:
-        ## calculate the index for key value
-        curr = self.map[key%len(self.map)]
-        while curr.next:
-            if curr.next.key == key:
-                curr.next.val = value
-                return
-            curr = curr.next
-        curr.next = ListNode(key , value)    
-        
-
-    def get(self, key: int) -> int:
-        curr = self.map[key%len(self.map)].next
-        ##.next because we need to start from dummy node insted start from 2nd node
-        while curr:
-            if curr.key == key:
-                return curr.val
-            curr = curr.next
-            
-        return -1    
-        
-
-    def remove(self, key: int) -> None:
-        curr = self.map[key%len(self.map)]
-        while curr and curr.next:
-            if curr.next.key == key:
-                curr.next = curr.next.next
-                return
-            curr = curr.next
-        
-
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
