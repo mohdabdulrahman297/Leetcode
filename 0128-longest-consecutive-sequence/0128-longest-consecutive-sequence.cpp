fe06b58b582cf7b242e6167f21edb28e542dbdb0
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-12 13:41:21 +0530

    Time: 381 ms (33.43%), Space: 60.9 MB (65.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -21,3 +21,20 @@ public:
         
     }
 };
+
+/*
+int main() {
+    // Example input
+    std::vector<int> nums = {100, 4, 200, 1, 3, 2};
+
+    Solution solution;
+    int result = solution.longestConsecutive(nums);
+    std::cout << "Input: ";
+    for (int num : nums) {
+        std::cout << num << " ";
+    }
+    std::cout << "\nLongest Consecutive Length: " << result << std::endl;
+    
+    return 0;
+}
+*/
