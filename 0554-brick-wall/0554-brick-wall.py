13bc97789f32d9c8e0a15f1381faa1b0c2df928c
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-26 20:19:19 +0530

    Time: 171 ms (78.24%), Space: 21.4 MB (21.23%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.py
+++ 0554-brick-wall/0554-brick-wall.py
@@ -15,3 +15,18 @@ class Solution:
                 
         return len(wall) - max(countGap.values())      
         
+# Create an instance of the Solution class
+##solution = Solution()
+
+# Example wall
+##wall = [
+##    [1, 2, 2, 1],
+##    [3, 1, 2],
+##    [1, 3, 2],
+##    [2, 4],
+##    [3, 1, 2],
+##    [1, 3, 1, 1]
+##]
+
+##result = solution.leastBricks(wall)
+##print(result)  # Debug print to see the final result##        
