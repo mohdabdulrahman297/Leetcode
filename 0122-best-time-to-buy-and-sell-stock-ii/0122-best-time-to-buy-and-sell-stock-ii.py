b326a35c09f772eb0e2e5c09836b04d646329b83
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-28 12:12:02 +0530

    Time: 70 ms (54.16%), Space: 17.7 MB (60.52%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,14 @@
+## space: o(1)
+## time : o(n)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit =0
+        
+        ## interate from the first value in arrays beacuse we have to compare the previous value for the current ith , hence 0th value dosen't have any  previous value , start the iteration from 1st place
+        for i in range(1 , len(prices)):
+            ## the above conditional statement inidicates profit
+            if(prices[i] > prices[i-1]):
+                profit += (prices[i] - prices[i-1])
+                
+        return profit        
+        
