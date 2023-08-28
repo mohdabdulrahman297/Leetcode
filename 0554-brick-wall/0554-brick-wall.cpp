28084058e572cf52139742b69f7bb840bdbbcc79
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-28 11:44:58 +0530

    Time: 29 ms (96.94%), Space: 21.6 MB (85.38%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -1,38 +1,40 @@
-//Time complexity : O(n x m)
-//Space complexity: O(n x m)
-
+// Time complexity: O(n x m)
+// Space complexity: O(n x m)
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
         
-        unordered_map<int , int> countGap;
+        // Create a hashmap to count gaps in each row
+        unordered_map<int, int> countGap;
         
-        int rows = wall.size();
-        int cols;
-        int maxGap = 0;
+        int rows = wall.size();  // Get the number of rows in the wall
+        int cols;  // Initialize variable to store the number of columns in each row
+        int maxGap = 0;  // Initialize a variable to track the maximum gap count
         
-        for(int i=0;i<rows;i++){
-            int total = 0;
+        // Iterate through each row in the wall
+        for (int i = 0; i < rows; i++) {
+            int total = 0;  // Initialize a variable to keep track of the current total
             
-            // '-1' because edge of the wall is not considered
-            cols = wall[i].size() - 1;
+            // '-1' because the edge of the wall is not considered
+            cols = wall[i].size() - 1;  // Calculate the number of columns in the current row
             
-            for(int j=0;j<cols;j++){
-                total += wall[i][j];
+            // Iterate through the columns in the current row
+            for (int j = 0; j < cols; j++) {
+                total += wall[i][j];  // Increment the total with the current brick's width
                 
-                if(countGap.find(total) != countGap.end()){
-                    countGap[total]++;
-                }
-                else{
-                    countGap[total] = 1;
+                // Check if the current total already exists in the hashmap
+                if (countGap.find(total) != countGap.end()) {
+                    countGap[total]++;  // If yes, increment the gap count at that position
+                } else {
+                    countGap[total] = 1;  // If not, create a new entry with gap count 1
                 }
                 
-                maxGap = max(maxGap , countGap[total]);
+                // Update the maximum gap count with the current gap count
+                maxGap = max(maxGap, countGap[total]);
             }
-            
-            
         }
-        return rows - maxGap;
         
+        // Return the difference between the total number of rows and the maximum gap count
+        return rows - maxGap;
     }
 };
