fce74b22764f25966a03194fa8ff05345deb0a41
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-30 14:04:28 +0530

    Time: 1789 ms (76.81%), Space: 16.5 MB (65.34%) - LeetHub

---
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -0,0 +1,34 @@
+## time: o(2^n)
+## space: o(2^n)
+class Solution:
+    def maxProduct(self, s: str) -> int:
+        
+        n = len(s);
+        
+        ##create a hashmap as follows
+        
+        pali = {} # bitmask of sub pailndrome : length of that palindrome
+        
+        for mask in range(1 , 1<<n): # 1<<n == 1<<2**n
+            
+            ## create an empyty string to convert back the bitmask
+            subseq = ""
+            for i in range(n):
+                
+                ## the above line inidcates that and operation gives 1 
+                if(mask & (1<<i)):
+                    subseq += s[i];
+            ## check if the subsequnce string is palindrome        
+            if subseq == subseq[::-1]:
+                ##then assing the key and value in hashmap
+                pali[mask] = len(subseq)
+                
+                
+        res = 0
+        
+        for m1 in pali:
+            for m2 in pali:
+                if(m1 & m2) == 0: #disjoint
+                    res = max(res , pali[m1] * pali[m2])
+        return res         
+                
