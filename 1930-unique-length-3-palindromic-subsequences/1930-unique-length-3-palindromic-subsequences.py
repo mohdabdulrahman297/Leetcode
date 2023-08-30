7a009e1f7d7f28c38cab0383b7cc03c3b4503068
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-30 07:12:16 +0530

    Time: 120 ms (99.16%), Space: 17.2 MB (72.13%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,31 @@
+## time: o(n^2)
+## space:o(n)
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        
+        ## initialize count variable for counting unique characters in input
+        count = 0
+        
+        ## use set() for extracting unique characters from the  input string
+        
+        chars = set(s)
+        
+        ## iterate
+        for char in chars:
+            
+            
+            # s.find(char) gets the index of first occurence of character and s.rfind(char) gets the index in reverse order i.e last 
+            
+            #For 'a', first would be 0 and last would be 3.
+            #For 'b', first would be 1 and last would be 1.
+            #For 'c', first would be 2 and last would be 2.
+            first = s.find(char)
+            last  = s.rfind(char)
+            
+            count += len(set(s[first+1:last]))
+            
+            
+        return count      
+                         
+                         
+        
