be1c66bc6801c44e00ce88a800b98d7978cc5855
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-08-30 13:09:18 +0530

    Time: 1420 ms (92.67%), Space: 72.6 MB (91.07%) - LeetHub

---
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -0,0 +1,24 @@
+##Time complexity : O(n)
+##Space complexity: O(n)
+class Solution:
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        
+        ## create a hashmap as follows
+        
+        count = {} # {w/h : count}
+        
+        res = 0
+        
+        for w , h in rectangles:
+            
+            #increament the count for ratio
+            
+            count[w/h] = count.get(w/h , 0) + 1
+            
+            #  we calculate the number of pairs that can be formed using rectangles with the same aspect ratio.
+            
+        for c in count.values():
+            res += (c*(c-1)) // 2
+            
+        return res    
+        
