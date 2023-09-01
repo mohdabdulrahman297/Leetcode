d6abc73328bb0a99b2d34ed7b5cf8f9771fdc017
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-01 22:17:25 +0530

    Time: 82 ms (92.70%), Space: 16.4 MB (73.97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,16 +2,23 @@
 ## space:o(26)
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # Create a hashmap to count occurrences of each character
         count = {}
-        
+        maxF = 0  # Initialize max frequency to 0
         l = 0
-        maxf = 0
+        
         for r in range(len(s)):
+            # Count occurrences of character s[r]
             count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
-
-            if (r - l + 1) - maxf > k:
+            
+            # Update max frequency within the window
+            maxF = max(maxF, count[s[r]])
+            
+            # If the length of the window minus the max frequency is greater than k,
+            # we need to shrink the window
+            if (r - l + 1) - maxF > k:
                 count[s[l]] -= 1
                 l += 1
-
-        return (r - l + 1)
+                
+        # The result is the length of the valid window
+        return r - l+1
