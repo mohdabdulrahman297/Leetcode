3846ea63d55fc12ee11e25fd9a4bd5896d086c2c
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-01 08:17:06 +0530

    Time: 67 ms (25.29%), Space: 17 MB (86.50%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,51 @@
+## space:o(n)
+## time:o(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # create two pointers left and right for the string
+        
+        l,r = 0 , len(s)-1
+        
+        ## both pointers should be in bound
+        
+        while(l<r):
+            
+            ## check if pointers are in bound(range) and not alphanumeric
+            
+            while l < r and not self.alphaNum(s[l]):
+                
+                l+=1
+                
+            while l < r and not self.alphaNum(s[r]):
+                
+                r-=1
+                
+            ## now check if first and last character are equal after converting them into lower case
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            ## update the pointers
+            l,r = l+1 , r-1
+            
+        return True    
+                
+            ## now check if both pointer values    
+        
+        
+        
+        
+        
+        
+        
+    ## create a function for checking alphnumeric or not
+    def alphaNum(self,c):
+        
+        ## ord method return the ascii value of the character
+        
+        
+        ##check if the value lies between ascii values of uppercase , lowercase , and integers 0 to 9
+        return (ord('A') <= ord(c) <= ord('Z')
+               or ord ('a') <= ord(c) <= ord('z')
+               or ord('0') <= ord(c) <= ord('9'))
+        
