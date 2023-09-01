5ac1be1895addc3f856c5e1bb9547e0bdde04042
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-01 20:25:01 +0530

    Time: 100 ms (61.21%), Space: 93.4 MB (12.00%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,38 +1,23 @@
-# space: o(1)
-# time: o(n)
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
         
-        ##left pointer indicates stock buy and right indicates stock sell(basic rule for profit)
-        l,r = 0 , 1
+        int l = 0;
+        int r = 1;
         
-        maxProfit = 0
+        int maxProfit = 0;
         
-        ##because the left will be at the minimum and the right is going to shift
-        while(r < len(prices)):
-            if(prices[l] < prices[r]):
-                profit = prices[r] - prices[l]
-                
-                maxProfit = max(maxProfit , profit)
-            else:
-                ## l=r because if at any instance l is increamented and it reahces a value greater than right then theres no profit
-                
-                l=r
-            r+=1    
-            
-        return maxProfit   
-    
-    
-    
-# Create an instance of the Solution class
-##solution = Solution()
-
-# Define an input list of stock prices
-##prices = [7, 1, 5, 3, 6, 4]
-
-# Call the maxProfit function with the input list
-##result = solution.maxProfit(prices)
-
-# Print the result
-#print("Maximum profit:", result)##    
+        while( r < prices.size()){
+            if(prices[l] < prices[r]){
+                int profit = prices[r] - prices[l];
+                maxProfit = max(maxProfit , profit);
+            }
+            else{
+                l=r;
+            }
+            r+=1;
+        }
+        return maxProfit;
         
+    }
+};
