d34d6697b2c5f2c12a99ef53ff7fbde25029daf2
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-01 18:48:10 +0530

    Time: 619 ms (50.06%), Space: 29.4 MB (43.76%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -24,5 +24,18 @@ class Solution:
                 ##this case exists for both pointer values to be equal no matter increase l or decrease r
                 l+=1
                 
-        return res      
+        return res
+    
+    
+# Create an instance of the Solution class
+#solution = Solution()
+
+# Define an input list of heights
+#heights = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+# Call the maxArea function with the input list
+#result = solution.maxArea(heights)
+
+# Print the result
+#print("Maximum area:", result)#    
         
