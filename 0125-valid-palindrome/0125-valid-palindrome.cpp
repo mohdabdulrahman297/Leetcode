b39da1fd3dc0de3cddcc8b7668383a5ae3f57de8
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-01 08:54:42 +0530

    Time: 8 ms (32.89%), Space: 7.3 MB (60.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -29,3 +29,19 @@ private:
         return (isalpha(c) || isdigit(c));
     }
 };
+
+/*
+int main() {
+    Solution solution;
+
+    std::string input1 = "A man, a plan, a canal: Panama";
+    bool result1 = solution.isPalindrome(input1);
+    std::cout << "Is \"" << input1 << "\" a palindrome? " << std::boolalpha << result1 << std::endl;
+
+    std::string input2 = "race a car";
+    bool result2 = solution.isPalindrome(input2);
+    std::cout << "Is \"" << input2 << "\" a palindrome? " << std::boolalpha << result2 << std::endl;
+
+    return 0;
+}
+*/
