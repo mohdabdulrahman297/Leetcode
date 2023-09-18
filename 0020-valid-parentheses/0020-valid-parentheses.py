58f140e9d33d97aee2cfc9c865dfeb3d0e74cec8
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-18 12:18:00 +0530

    Time: 36 ms (77.70%), Space: 16.2 MB (97.97%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,35 @@
+## space: o(n)
+## time: o(n)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        ## create an empty stack
+        stack = []
+        ## map the closing brackets to open brackets using hashmaps
+        
+        closeToOpen = { ")" : "(" , "]" : "[" , "}" : "{"}
+        
+        ##iterate through the input string and check if the key(closing bracket in the stack) -1 is the last element of stack
+        
+        for c in s:
+            if c in closeToOpen:
+                
+                if stack and stack[-1]  == closeToOpen[c]:
+                    ## start popping
+                    stack.pop()
+                    
+                else:
+                    ## means there is no matching 
+                    return False
+                
+            else:
+                ## this else case inidcates the brakcet is open and hence we add it to the stack stack follows LIFO order
+                stack.append(c)
+                
+        if not stack:
+            return True
+        else:
+            return False
+            
+        
