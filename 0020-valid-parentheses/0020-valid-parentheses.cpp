1ea6d5e6f0c2b73a35a07cd769ed5e6bfaf02fd7
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-18 12:25:58 +0530

    Time: 3 ms (43.73%), Space: 6.7 MB (6.50%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -23,3 +23,20 @@ public:
         
     }
 };
+
+/*
+int main() {
+    Solution solution;
+
+    // Test cases
+    std::string input_str1 = "()";
+    std::string input_str2 = "({[()]})";
+
+    // Check if the input strings have valid brackets
+    std::cout << std::boolalpha;  // Print "true" or "false" instead of 1 or 0
+    std::cout << solution.isValid(input_str1) << std::endl;  // Should print true
+    std::cout << solution.isValid(input_str2) << std::endl;  // Should print true
+
+    return 0;
+}
+*/
