1be1ee987c45be9cd9f9fb5c944623c79c96478a
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-09-18 14:36:47 +0530

    Time: 81 ms (26.07%), Space: 16.5 MB (99.88%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,38 @@
+## space: o(n)
+## time: o(n)
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        ##create an empty stack
+        stack = []
+        
+        ## for different arthmetic operators apply the logic
+        
+        for c in tokens:
+            if c == "+":
+                ## pop those two values before the operator , apply the operation between them and add the result to the stack
+                
+                stack.append(stack.pop() +  stack.pop())
+                
+            elif c == "-":
+                ## b-a gives positive values whereas a-b gives negative
+                a,b = stack.pop(), stack.pop()
+                stack.append(b-a)
+                
+            elif c == "*":
+                
+                stack.append(stack.pop() * stack.pop())
+                
+            elif c ==  "/":
+                a,b = stack.pop(), stack.pop()
+                ##int() rounds off the number
+                stack.append(int(b/a))
+                
+            else:
+                ## first convert string into int and then append 
+                stack.append(int(c))
+        
+        ## the element that is left in the stack
+        return stack[0]         
+        
