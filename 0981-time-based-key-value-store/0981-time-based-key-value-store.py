0d03c162df403cdc86bb56d1f2f41fb94f5b4a9e
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-10-15 20:02:36 +0530

    Time: 694 ms (35.24%), Space: 74.8 MB (24.13%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,46 @@
+## space: o(n)
+## time: o(logn)
+class TimeMap:
+
+    def __init__(self):
+        
+        ## initialize your data strucutre
+        self.keyStore = {} # key : list of [val, timestamp]
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        
+        ##first check if that key already exists in the hashmap
+        if key not in self.keyStore:
+            ## initialize an empty list for that key and then append the inputs to it
+            self.keyStore[key] = []
+            
+        self.keyStore[key].append([value, timestamp])    
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        ## initialize an empty result string and and the list of lists for the key
+        res = ""
+        values = self.keyStore.get(key, [])
+        ##initialize the pointers
+        l, r = 0, len(values) - 1
+        while(l <= r):
+            m = (l+r)//2
+            ## check if the values list that contains the value and timestamp is less than the input timestamp
+            if(values[m][1] <= timestamp):
+                ##update the res string (update the value which is at values[m][0] and timestamp at values[m][1])
+                res = values[m][0]
+                ## increament the pointer
+                l = m+1
+            else:
+                r = m-1
+                
+        return res       
+                
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
