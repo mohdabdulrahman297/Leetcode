f511ef8130c82536a65c2889e6f6e8bf1096be2b
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-10-15 16:21:49 +0530

    Time: 6 ms (22.63%), Space: 10.6 MB (14.43%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,35 @@
+// space : o(1)
+// time: o(logn)
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        // Neetcode solution Ologn time O1 space binary search
+        int res = nums[0];
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])
+            {
+                res = min(res, nums[l]);
+                break;
+            }
+            int mid = l + (r - l) / 2;
+            res = min(res, nums[mid]);
+
+            if (nums[mid] >= nums[l]) // mid present in left sorted array
+            {
+                l = mid + 1; // try to move closer to right sorted array
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+};
