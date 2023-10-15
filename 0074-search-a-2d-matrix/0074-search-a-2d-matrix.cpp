fb575743ae7c0cefea8385c8e3e9a47451c7c2af
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-10-15 13:21:31 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (40.43%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,42 +1,47 @@
+// space: o(1)
+// time: o(logm + logn)
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int ROWS = matrix.size();
         int COLS = matrix[0].size();
-        int top = 0, bot = ROWS - 1;
-
-        while (top <= bot) {
-            int row = (top + bot) / 2;
-
-            if (target > matrix[row][COLS - 1]) {
+        int top = 0;
+        int bot = ROWS - 1;
+        
+        while(top <= bot){
+            int row = (top + bot)/ 2;
+            
+            if(target > matrix[row][COLS - 1]){
                 top = row + 1;
-            } else if (target < matrix[row][0]) {
+            }
+            else if(target < matrix[row][0]){
                 bot = row - 1;
-            } else {
+            }
+            else{
                 break;
             }
         }
-
-        if (!(top <= bot)) {
+        
+        if(!(top <= bot)){
             return false;
         }
-
-        int row = (top + bot) / 2;
+        
+        int row = (top + bot)/2;
         int l = 0;
         int r = COLS - 1;
-
-        while (l <= r) {
-            int m = (l + r) / 2;
-
-            if (target > matrix[row][m]) {
-                l = m + 1;
-            } else if (target < matrix[row][m]) {
-                r = m - 1;
-            } else {
+        
+        while(l <= r){
+            int m = (l+r)/2;
+            if(target > matrix[row][m]){
+                l = m+1;
+            }
+            else if(target < matrix[row][m]){
+                r = m-1;
+            }
+            else{
                 return true;
             }
         }
-
         return false;
     }
 };
