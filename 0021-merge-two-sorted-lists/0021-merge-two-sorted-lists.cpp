a3a7c097f32612da806ad813b276ce6ee648d548
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-12-06 13:38:35 +0530

    Time: 0 ms (100.00%), Space: 15 MB (99.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -17,35 +17,22 @@ struct ListNode {
 };
 
 
- * ITERATIVE SOLUTION
+ * RECURSIVE SOLUTION
  */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode();
-        ListNode* tail = dummy;
+       if(!list1){
+           return list2;
+       }
+       if(!list2){
+           return list1;
+       }
         
-        while(list1 && list2){
-            if(list1->val < list2->val){
-                tail->next = list1;
-                list1 = list1->next;
-            }
-            else{
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            
-            tail = tail->next;
-        }
+       ListNode*lil = (list1->val < list2->val) ? list1 : list2;
+       ListNode*big = (list1->val < list2->val) ? list2 : list1;
+       lil->next = mergeTwoLists(lil->next, big);
         
-        if(list1){
-            tail->next = list1;
-        }
-        else if(list2){
-            tail->next = list2;
-        }
-        
-        // because the first node is dummy and the next chain of it         // is our answer
-        return dummy->next;
+       return lil;
     }
 };
