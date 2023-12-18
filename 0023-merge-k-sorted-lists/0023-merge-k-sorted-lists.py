924f4e1d12c68254a942ea0caa53e7ce8b43f5e5
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-12-18 23:44:01 +0530

    Time: 112 ms (27.58%), Space: 20 MB (65.64%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        ## if the list is empty 
+        if not lists or len(lists) == 0:
+            return None
+        
+        ## iterate until there's one linked list left in the array of linked lists(cutting in half)
+        
+        while len(lists) > 1:
+            mergedLists = []
+            
+            ## now iterate through each of these lists
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                ## check for non null
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists    
+        return lists[0]    
+        
+    ## merge two lists    
+    def mergeList(self, l1, l2):
+        # todo
+        ## create a dummy node
+        dummy = ListNode()
+        tail = dummy
+        
+        ## iterate until non null
+        while l1 and l2:
+            
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next    
