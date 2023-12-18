9cf9966270bee5a0b0112d598661c079633d8fa2
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-12-18 23:44:48 +0530

    Time: 101 ms (44.47%), Space: 19.9 MB (82.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# time : o(nlogk)
+# space : o(n)
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
