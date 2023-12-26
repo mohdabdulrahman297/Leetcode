becd00135ab7d505e28cf2dbe81a5b2fee448fbb
mohdabdulrahman297 <109975518+mohdabdulrahman297@users.noreply.github.com>
2023-12-26 11:37:05 +0530

    Time: 3 ms (60.65%), Space: 12.3 MB (80.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -15,33 +15,66 @@ class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> result;
-        if (!root) {
+        
+        if(!root){
             return result;
         }
-
+        
         queue<TreeNode*> q;
         q.push(root);
-
-        while (!q.empty()) {
+        
+        while(!q.empty()){
             TreeNode* rightSide = nullptr;
             int qSize = q.size();
-
-            for (int i = 0; i < qSize; ++i) {
+            
+            for(int i=0; i<qSize; ++i){
                 TreeNode* node = q.front();
                 q.pop();
-
-                if (node) {
+                
+                if(node){
                     rightSide = node;
-                    if (node->left) q.push(node->left);
-                    if (node->right) q.push(node->right);
+                    if(node->left){
+                        q.push(node->left);
+                    }
+                    if(node->right){
+                        q.push(node->right);
+                    }
                 }
             }
-
-            if (rightSide) {
-                result.push_back(rightSide->val);
-            }
+            if(rightSide){
+                    result.push_back(rightSide->val);
+                }
         }
-
         return result;
     }
 };
+
+/*
+int main() {
+    // Example usage
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(4);
+
+    Solution solution;
+    vector<int> result = solution.rightSideView(root);
+
+    // Display the result
+    cout << "Right Side View: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Remember to free the allocated memory
+    delete root->left->right;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
+*/
